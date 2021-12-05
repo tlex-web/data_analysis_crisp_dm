e98c1ad7b1e1013b8042a47bf8482510351eb7c3
tlex-web <asterix2514@gmail.com>
2021-12-05 13:19:49 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# CRISP-DM with Python and Jupyter Notebook
---
+++ helpers/helper_functions.py
@@ -0,0 +1,103 @@
+# global import of modules prevent circular imports errors
+import pandas as pd
+import numpy as np
+
+import matplotlib as plt
+
+import plotly.graph_objects as go
+from plotly.missing_ipywidgets import FigureWidget
+from plotly.subplots import make_subplots
+import plotly.express as px
+import plotly.offline as py
+from plotly.offline import download_plotlyjs, init_notebook_mode, iplot, plot
+
+import missingno as msno
+
+
+def read_and_set_df(filepath: str) -> pd.DataFrame:
+    # Datensatz einlesen
+    df = pd.read_csv(filepath, sep="$", decimal=".")
+
+    # Spaltennamen alle kleingeschrieben
+    df.columns = df.columns.str.lower()
+    df.columns
+
+    # Die Spaltennamen waren verschoben - In diesem Schritt werden sie richtig zugeordnet
+    df.rename(columns={
+        'unnamed: 0': 'id',
+        'id': 'gender',
+        'gender': 'age',
+        'age': 'driving_license',
+        'driving_license': 'region_code',
+        'region_code': 'previously_insured',
+        'previously_insured': 'vehicle_age',
+        'vehicle_age': 'vehicle_damage',
+        'vehicle_damage': 'annual_premium',
+        'annual_premium': 'policy_sales_channel',
+        'policy_sales_channel': 'vintage',
+        'vintage': 'response',
+        'response': 'nan'
+    },
+        inplace=True)
+
+    # Letzte Spalte besteht nur aus nan und kann somit gelöscht werden
+    del df['nan']
+
+    return df
+
+
+def set_datatypes(df: pd.DataFrame) -> pd.DataFrame:
+    # Numerische Variablen
+    # Nullable Interger
+
+    # Variable Age
+    # Im ersten Schritt setzen wir alle Werte als String
+    df['age'] = df['age'].astype(str)
+    # Dann entfernen wir den doppelten Punkt in den betreffenden Werten
+    df['age'] = df['age'].str.rstrip('.')
+    # Age zu einem numerischen Datentyp casten
+    # coerce': Wenn coerece, dann wird ungültiges Parsing als NaN gesetzt
+    df["age"] = pd.to_numeric(df["age"], errors='coerce')
+    df['age'] = df['age'].astype('Int64')
+
+    # Annual Premium
+    df['annual_premium'] = df['annual_premium'].astype(str)
+    df['annual_premium'] = df['annual_premium'].str.rstrip('.')
+    df["annual_premium"] = pd.to_numeric(df["annual_premium"], errors='coerce')
+    df["annual_premium"] = df["annual_premium"].astype('Int64')
+    df['annual_premium'] = df['annual_premium'].astype('float')
+
+    # Vintage
+    df['vintage'] = df['vintage'].astype(str)
+    df['vintage'] = df['vintage'].str.rstrip('##')
+    df["vintage"] = pd.to_numeric(df["vintage"], errors='coerce')
+    df["vintage"] = df["vintage"].astype('Int64')
+
+    # Region Code
+    df['region_code'] = df['region_code'].astype(str)
+    df['region_code'] = df['region_code'].str.rstrip('#')
+    df["region_code"] = pd.to_numeric(df["region_code"], errors='coerce')
+    df['region_code'] = df['region_code'].astype('float')
+
+    # Policy Sales Channel
+    df['policy_sales_channel'] = df['policy_sales_channel'].astype(str)
+    df['policy_sales_channel'] = df['policy_sales_channel'].str.rstrip('##')
+    df["policy_sales_channel"] = pd.to_numeric(
+        df["policy_sales_channel"], errors='coerce')
+    df["policy_sales_channel"] = df["policy_sales_channel"].astype('Int64')
+
+    # Kategorische Variablen
+    df['gender'] = df['gender'].astype('category')
+    df['driving_license'] = df['driving_license'].astype('category')
+    df['previously_insured'] = df['previously_insured'].astype('category')
+    df['vehicle_damage'] = df['vehicle_damage'].astype('category')
+    df['vehicle_age'] = df['vehicle_age'].astype('category')
+
+    # Response
+    df.response.replace(
+        {'0': 'no', '1': 'yes', 1: 'yes', 0: 'no'}, inplace=True)
+    df['response'] = df['response'].astype('category')
+
+
+def get_plotly_colors() -> FigureWidget:
+    return px.colors.sequential.swatches()
